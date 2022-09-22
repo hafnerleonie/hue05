0b5efcae4d14b3ecc03de519c69396fce3367f14
hafnerleonie <hafner.leonie.2006@gmx.at>
2022-09-22 11:35:15 +0200

    Initial commit

---
+++ src/net/htlgkr/hafle/pos3/eratosthenesPrimeSieve/EratosthenesPrimeSieve.java
@@ -0,0 +1,43 @@
+package net.htlgkr.hafle.pos3.eratosthenesPrimeSieve;
+
+public class EratosthenesPrimeSieve implements  PrimeSieve {
+    static int n = 100;
+    boolean[] isPrime = new boolean[n + 1];
+    int[] onlyPrimes = new int[n];
+    int counter = 0;
+
+    @Override
+    public boolean isPrime(int p) {
+
+        for (int i = 2; i <= p; i++) {
+            isPrime[i] = true;
+        }
+        for (int factor = 2; factor <= p; factor++) {
+
+            if (isPrime[factor]) {
+                onlyPrimes[counter] = factor;
+                counter++;
+                for (int j = 2; factor * j <= n; j++) {
+                    isPrime[factor * j] = false;
+                }
+            }
+        }
+        return isPrime[p];
+    }
+
+        @Override
+        public void printPrimes (){
+        int primes = 0;
+            for (int i = 2; i <= n; i++) {
+                if (isPrime[i]) primes++;
+            }
+            System.out.println("The number of primes <= " + n + " is " + primes);
+        }
+
+    public static void main(String[] args) {
+        EratosthenesPrimeSieve e = new EratosthenesPrimeSieve();
+        e.isPrime(n);
+        e.printPrimes();
+
+    }
+}
---
+++ src/net/htlgkr/hafle/pos3/eratosthenesPrimeSieve/PrimeSieve.java
@@ -0,0 +1,6 @@
+package net.htlgkr.hafle.pos3.eratosthenesPrimeSieve;
+
+public interface PrimeSieve {
+    public boolean isPrime(int p);
+    public void printPrimes();
+}
